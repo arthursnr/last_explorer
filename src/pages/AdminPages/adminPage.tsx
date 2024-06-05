d7e6d7dfffb2e09494dacfa13753c239dfe42146
Arthur Ribeiro <arthursnr2021@gmail.com>
2024-06-05 19:07:49 -0300

    admin main page OK

--- src/pages/AdminPages/adminPage.tsx
+++ src/pages/AdminPages/adminPage.tsx
@@ -1,7 +1,68 @@
+import { MealsPage } from "./mealsPage";
 
 
 export function AdminPage() {
-    return(
-        <div></div>
+
+    return (
+        <div className='bg-black min-w-screen min-h-screen'>
+            <div className="bg-slate-950 w-screen h-28">
+                <header className="flex justify-between items-center pt-14 pb-6 px-7 w-screen gap-4">
+                    <button>
+                        <img src="src/assets/more.svg" alt="" className="h-5 w-6" />
+                    </button>
+                    <ul className="flex-grow flex flex-row justify-center text-white gap-4">
+                        <div className="flex text-xl text-white justify-center items-center gap-2">
+                            <img src="src/assets/image.png" alt="logo" className='size-6' />
+                            <h1 className='font-bold'>
+                                food explorer
+                            </h1>
+                            <p className="text-cyan-300 text-xs font-normal leading-tight">admin</p>
+                        </div>
+                    </ul>
+                </header>
+            </div>
+
+            <div className="relative flex justify-center mt-11 mr-4 ml-7 mb-16 rounded-sm bg-gradient-to-b from-slate-900/90 to-slate-950 h-[120px]">
+                <img src="src/assets/image1.png" className="absolute left-[-30px] top-[-30px] z-10 h-[149px] w-[191px]" />
+                <div className="text-white ml-36 mt-9 mb-5 self-center">
+                    <h2>Sabores inigualáveis</h2>
+                    <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
+                </div>
+            </div>
+            <div className="ml-6">
+                <div className="flex flex-col gap-6 text-white h-[340px] min-w-screen">
+                    <h2>Refeições</h2>
+                    <div>
+                        <MealsPage />
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-6 text-white h-[340px]">
+                    <h2>Pratos Principais</h2>
+                    <div>
+                        <div>PRODUTO 1</div>
+                        <div>PRODUTO 2</div>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-6 text-white h-[340px]">
+                    <h2>Pratos Principais</h2>
+                    <div>
+                        <div>PRODUTO 1</div>
+                        <div>PRODUTO 2</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="bg-slate-950 w-screen flex items-center justify-center">
+                <footer className="py-6 bg-slate-950 flex justify-center items-center">
+                    <ul className="text-white flex items-center gap-2">
+                        <img src="src/assets/Polygon 1 (1).png" alt="Logo" className="mb-2" />
+                        <p>food explorer</p>
+                        <p>© 2024 - Todos os direitos reservados.</p>
+                    </ul>
+                </footer>
+            </div>
+        </div>
     )
 }
---
+++ src/pages/AdminPages/mealsPage.tsx
@@ -0,0 +1,52 @@
+export function MealsPage() {
+    return (
+        <div>
+            <div className="flex gap-4 overflow-x-auto">
+                <div className="  p-6   border border-black flex-col justify-center items-center gap-3 inline-flex">
+                    <div className="size-[88px] justify-center items-center inline-flex">
+                        <img src="src\assets\Mask group.png" alt="Imagem de Salada Ravanello" className=" size-[88px] justify-center items-center inline-flex" />
+                    </div>
+                    <div className="self-stretch text-center text-zinc-200 text-sm font-medium leading-normal">
+                        Salada Ravanello &gt;
+                    </div>
+                    <div className="text-center text-cyan-300 text-base font-normal leading-none">
+                        R$ 49,79
+                    </div>
+                </div>
+                <div className="  p-6   border border-black flex-col justify-center items-center gap-3 inline-flex">
+                    <div className="size-[88px] justify-center items-center inline-flex">
+                        <img src="src\assets\Mask group.png" alt="Imagem de Salada Ravanello" className=" size-[88px] justify-center items-center inline-flex" />
+                    </div>
+                    <div className="self-stretch text-center text-zinc-200 text-sm font-medium leading-normal">
+                        Salada Ravanello &gt;
+                    </div>
+                    <div className="text-center text-cyan-300 text-base font-normal leading-none">
+                        R$ 49,79
+                    </div>
+                </div>
+                <div className="  p-6   border border-black flex-col justify-center items-center gap-3 inline-flex">
+                    <div className="size-[88px] justify-center items-center inline-flex">
+                        <img src="src\assets\Mask group.png" alt="Imagem de Salada Ravanello" className=" size-[88px] justify-center items-center inline-flex" />
+                    </div>
+                    <div className="self-stretch text-center text-zinc-200 text-sm font-medium leading-normal">
+                        Salada Ravanello &gt;
+                    </div>
+                    <div className="text-center text-cyan-300 text-base font-normal leading-none">
+                        R$ 49,79
+                    </div>
+                </div>
+                <div className="  p-6   border border-black flex-col justify-center items-center gap-3 inline-flex">
+                    <div className="size-[88px] justify-center items-center inline-flex">
+                        <img src="src\assets\Mask group.png" alt="Imagem de Salada Ravanello" className=" size-[88px] justify-center items-center inline-flex" />
+                    </div>
+                    <div className="self-stretch text-center text-zinc-200 text-sm font-medium leading-normal">
+                        Salada Ravanello &gt;
+                    </div>
+                    <div className="text-center text-cyan-300 text-base font-normal leading-none">
+                        R$ 49,79
+                    </div>
+                </div>
+                </div>
+            </div>
+            )
+}
